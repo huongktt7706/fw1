643407adff0aa0a53f4f8bdd7c38f4d326854bd1
huongktt7706 <thuyhuong7706nb@gmail.com>
2026-03-12 12:50:57 +0700

    routerLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   // imports: [],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
 })
 export class App {
