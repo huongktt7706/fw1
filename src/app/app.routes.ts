95cbf794a6a8d34b2372f7bab487ac56d7313e22
huongktt7706 <thuyhuong7706nb@gmail.com>
2026-03-14 12:57:37 +0700

    10 diem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
+
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
+import { Stories } from './pages/stories/stories';
+
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: 'home', component: Home },
   { path: 'about', component: About },
+  {path:'stories', component:Stories}
+ 
+//   { path: 'products/laptop', component: Products },
 ];
